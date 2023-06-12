a6c4bf7784ba082376a117b7e53c12e021eec694
LopezDev <aragonlopezjuan87@gmail.com>
2023-06-12 06:58:50 +0200

    feat : create component auth google

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
 import './index.css'
+
+const CLIENT_ID = '679933917994-8jitnb84e28j1ma5cm2su7ap89v578u3.apps.googleusercontent.com'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
     <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 )
--- src/page/auth/auth.tsx
+++ src/page/auth/auth.tsx
@@ -1,16 +1,33 @@
 import React, { useEffect } from 'react'
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+import { GoogleLogin } from '@react-oauth/google'
 
-const CLIENT_ID = '679933917994-doms8a9i88vjtg54nkvr31fq08og85vk.apps.googleusercontent.com'
+const checkIsUserLoggedIn = () => {
+  const accessToken = localStorage.getItem('access_token')
+  return !!accessToken // Devuelve true si hay un token de acceso, de lo contrario, devuelve false
+}
+const CLIENT_ID = '679933917994-8jitnb84e28j1ma5cm2su7ap89v578u3.apps.googleusercontent.com'
+
+const handleLoginSuccess = (credentialResponse:any) => {
+  // Almacenar el token de acceso en el almacenamiento local
+
+  const token = JSON.stringify(credentialResponse)
+  localStorage.setItem('access_token', token)
+  console.log('Inicio de sesión exitoso')
+}
+
+const handleLoginFailure = () => {
+  console.log('Error al iniciar sesión')
+}
 
 export const AuthGoogle = () => {
-  const handleOnSuccess = res => console.log(res)
-  return (
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
+  if (checkIsUserLoggedIn()) {
+    return <div>El usuario ya ha iniciado sesión</div>
+  } else {
+    return (
       <GoogleLogin
-        onSuccess={() => handleOnSuccess}
-        onError={() => console.log('err')}
+        onSuccess={handleLoginSuccess}
+        onFailure={handleLoginFailure}
       />
-    </GoogleOAuthProvider>
-  )
+    )
+  }
 }
